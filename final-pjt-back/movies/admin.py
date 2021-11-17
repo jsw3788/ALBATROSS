1272dc1169603bead8f8418ee06dad50cb498a56
양지훈 <kelvin9149@gmail.com>
2021-11-17 14:57:26 +0900

    add admin(superuser)

--- final-pjt-back/movies/admin.py
+++ final-pjt-back/movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Actor, Director, Genre, Movie, Recommend, Record, Review
 
 # Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Director)
+admin.site.register(Actor)
+admin.site.register(Review)
+admin.site.register(Record)
+admin.site.register(Recommend)
