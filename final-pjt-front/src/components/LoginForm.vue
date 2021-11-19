6d3c854a66884bcfb6bd2cf257b5d21da7a616e5
양지훈 <kelvin9149@gmail.com>
2021-11-19 13:15:50 +0900

    accounts: signup & login update

---
+++ final-pjt-front/src/components/LoginForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <div>
+      <label for="username">ID : </label>
+      <input type="text" id="username" v-model="credentials.username" />
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input
+        type="password"
+        id="password"
+        v-model="credentials.password"
+        @keyup.enter="login"
+      />
+    </div>
+    <button @click="login">로그인</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data: function () {
+    return {
+      credentials: {
+        username: null,
+        password: null,
+      },
+    };
+  },
+  methods: {
+    login: function () {
+      axios({
+        method: "post",
+        url: `${process.env.VUE_APP_SERVER_URL}/api/v2/api-token-auth/`,
+        data: this.credentials,
+      })
+        .then((res) => {
+          console.log(res);
+          localStorage.setItem("jwt", res.data.token);
+          // this.$emit("login");
+          this.$router.go();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
