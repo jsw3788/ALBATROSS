ffc7689ef56b7f402d5e38c007126fea65f9d1a6
jsw3788 <jsw3788@naver.com>
2021-11-24 01:42:04 +0900

    query optimizing

--- final-pjt-back/server/urls.py
+++ final-pjt-back/server/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('movies.urls')),
     path('api/v2/', include('accounts.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
