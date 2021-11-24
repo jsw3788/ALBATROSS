6e06acf8afbd3a1d8403db0b0d3f62e0bcbb5834
jsw3788 <jsw3788@naver.com>
2021-11-24 21:43:22 +0900

    update index

--- final-pjt-front/src/views/Index.vue
+++ final-pjt-front/src/views/Index.vue
@@ -24,32 +24,34 @@ export default {
     }
   },
   mounted: function() {
+    setTimeout(()=> {
 
-    // anime
-    // Wrap every letter in a span
-    let textWrapper = document.querySelector('.ml9 .letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    
-    anime.timeline({loop: true})
-      .add({
-        targets: '.ml9 .letter',
-        scale: [0, 1],
-        duration: 1500,
-        elasticity: 600,
-        delay: (el, i) => 45 * (i+1)
-      }).add({
-        targets: '.ml9',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-      });
-
-      setTimeout(()=>{
-        this.$router.push({name: "Home"})
-        this.isIndex=false
-        this.$emit('goHome',isIndex)
-      }, 2000)
+      // anime
+      // Wrap every letter in a span
+      let textWrapper = document.querySelector('.ml9 .letters');
+      textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+      
+      anime.timeline({loop: true})
+        .add({
+          targets: '.ml9 .letter',
+          scale: [0, 1],
+          duration: 1500,
+          elasticity: 600,
+          delay: (el, i) => 45 * (i+1)
+        }).add({
+          targets: '.ml9',
+          opacity: 0,
+          duration: 1000,
+          easing: "easeOutExpo",
+          delay: 1000
+        });
+  
+        setTimeout(()=>{
+          this.$router.push({name: "Home"})
+          this.isIndex=false
+          this.$emit('goHome',isIndex)
+        }, 3000)
+    },1000)
 
   }
 
